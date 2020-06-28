e3ed2fd8b7c73625fdc32ebbde7e776bc1a4b9c3
NehaNishikant <neha1127@hotmail.com>
2020-06-27 20:52:58 -0400

    sample http post

--- application.py
+++ application.py
@@ -4,3 +4,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+import requests
+import json
+
+url="http://example.com/index.php"
+
+r = requests.post(url, params={'q': 'raspberry pi request'})
+
+
+
+if r.status_code != 200:
+
+  print "Error:", r.status_code
+
+
+
+data = r.json()
+
+example = data["value1"]["value2"]
+
+print(example)
