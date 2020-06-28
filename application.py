a6d119f1fc8c4ee59010eae8e4ad90038a1deb70
NehaNishikant <neha1127@hotmail.com>
2020-06-27 21:45:30 -0400

    index method

--- application.py
+++ application.py
@@ -5,22 +5,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
 
-def hello2():
-    return "Hello world2"
-
-def postrequest():
+def index():
     url1="https://api.postmates.com/v1/customers/cus_MjTrLMXWncD3Rk/delivery_quotes"
     url2="https://api.postmates.com/v1/customers/cus_MjTrLMXWncD3Rk/deliveries"
 
     #https://postmates.com/developer/docs/#resources__delivery__create-delivery
 
-    params = 
-    {
-      'dropoff_address': '30 Providence Blvd, Kendall Park NJ, 08824'
-      'pickup_address': '9 Bernadette Circle, Monmouth Junction, 08852'
+    params = {
+      "dropoff_address": "30 Providence Blvd, Kendall Park NJ, 08824",
+      "pickup_address": "9 Bernadette Circle, Monmouth Junction, 08852"
     }
 
     r = requests.post(url1, params=params)
@@ -30,3 +24,12 @@ def postrequest():
 
     data = r.json()
     return data
+
+
+'''
+def hello():
+    return "Hello World!"
+
+def hello2():
+    return "Hello world2"
+'''
